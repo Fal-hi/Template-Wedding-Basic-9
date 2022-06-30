5f078ae66058a938cc9362621fdadcb790fb55fd
Fal-hi <syaifal.illahi@gmail.com>
2022-06-30 21:08:25 +0700

    Maps telah diperbarui

--- public/js/script.js
+++ public/js/script.js
@@ -22,43 +22,6 @@ let x = setInterval(function () {
   document.getElementById("detik").innerHTML = seconds + " Detik";
 }, 1000);
 
-// Initialize and add the map
-function initMap() {
-  // The location
-  const akadNikah = { lat: -0.9225054940582557, lng: 100.35991095174292 };
-  const resepsi1 = { lat: -0.9225054940582557, lng: 100.35991095174292 };
-  const resepsi2 = { lat: -0.9225054940582557, lng: 100.35991095174292 };
-  // The map, centered the location
-  const map1 = new google.maps.Map(document.getElementById("akadNikah"), {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: akadNikah,
-  });
-  const map2 = new google.maps.Map(document.getElementById("resepsi1"), {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: resepsi1,
-  });
-  const map3 = new google.maps.Map(document.getElementById("resepsi2"), {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: resepsi2,
-  });
-  // The marker, positioned the location
-  const markerAkadNikah = new google.maps.Marker({
-    position: akadNikah,
-    map: map1,
-  });
-  const markerResepsi = new google.maps.Marker({
-    position: resepsi1,
-    map: map2,
-  });
-  const markerUnduhMantu = new google.maps.Marker({
-    position: resepsi2,
-    map: map3,
-  });
-}
-
 // Musik
 const track = document.getElementById("track");
 const button = document.getElementById("play-pause");
